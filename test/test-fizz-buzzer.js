082449a051e0286a6d23b5487134d29e83dede62
Nick Foden <nickfoden@gmail.com>
2017-04-13 00:02:24 -0400

    Adding tests

---
+++ test/test-fizz-buzzer.js
@@ -0,0 +1,30 @@
+const should = require('chai').should();
+const expect = require('chai').expect();
+const fizzBuzz = require('../fizzbuzzer');
+
+describe ('num', function() {
+		it('should be a number', function() {
+			const answer = fizzBuzz(7);
+			answer.should.be.a('number');
+		});
+		it('should be divisible by 15', function() {
+			const answer = fizzBuzz(45);
+			answer.should.equal('fizz-buzz');
+		});
+		it('should be divisble by 5', function() {
+			const answer = fizzBuzz(100);
+			answer.should.equal('buzz');
+		});
+		it('should be divisble by 3', function() {
+			const answer = fizzBuzz(9);
+			answer.should.equal('fizz');
+		});
+		it('should throw error', function() {
+				(fizzBuzz("green")).should.throw(Error)
+		});
+	});			
+
+
+
+
+
